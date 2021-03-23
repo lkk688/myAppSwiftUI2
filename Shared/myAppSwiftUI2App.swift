3ac7c8f775aaf92844d1952949949f72effaae93
Kaikai Liu <kaikai.liu@sjsu.edu>
2021-03-23 13:54:10 -0700

    Initial Commit

---
+++ Shared/myAppSwiftUI2App.swift
@@ -0,0 +1,17 @@
+//
+//  myAppSwiftUI2App.swift
+//  Shared
+//
+//  Created by Kaikai Liu on 3/23/21.
+//
+
+import SwiftUI
+
+@main
+struct myAppSwiftUI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
